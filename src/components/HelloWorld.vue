21b7f219147cb33c759cd1dc93559c57e853102c
JackXie60 <1809697652@qq.com>
2022-04-22 11:05:53 +0800

    feat: action

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and resdsdcipsdes on how to configure / customize this project,<br>
+      For a guide and resdsdsdcipsdes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
